56ec84a7a277caf68f1a7ac8f6650f1e8919dc55
Waldomiro Seabra <wjgs@icomp.ufam.edu.br>
2016-07-15 10:50:47 -0400

    barbeiro executa indefinidamente

--- barbeiro.c
+++ barbeiro.c
@@ -4,24 +4,32 @@
 #include <semaphore.h>
 #include <unistd.h>
 
+
+
 sem_t barbeiros_livres;
 sem_t cadeiras_ocupadas;
 int nBarbeiros;
 int nCadeiras;
 int nClientes;
 int nClientesAtendidos=0;
+int tipo;
+
+enum{ClienteRapido=1, BarbeiroRapido=2, Iguais=3};
 
 void *barbeiro(void *arg){
 	long tid = (long) arg;
-    while(nClientesAtendidos < nClientes){
-	printf("barbeiro %d vai esperar por clientes!!\n",tid);
-	sem_wait(&cadeiras_ocupadas); // espera por clientes
-	printf("barbeiro %d cortando cabelo\n",tid);
-	//corta cabelo
-	sleep(10);
-	sem_post(&barbeiros_livres);
-	printf("barbeiro %d está livre\n",tid);
-	//cortou o cabelo e agora está livre
+	while(1){
+		printf("barbeiro %d ",tid);
+		printf("vai esperar por clientes!!\n");
+		sem_wait(&cadeiras_ocupadas); // espera por clientes
+		printf("barbeiro %d cortando cabelo\n",tid);
+		//corta cabelo
+		if(tipo == ClienteRapido) sleep(10);
+		else if(tipo == BarbeiroRapido) sleep(1);
+		else sleep(1);
+		sem_post(&barbeiros_livres);
+		printf("barbeiro %d está livre\n",tid);
+		//cortou o cabelo e agora está livre
 	}
 }
 
@@ -47,23 +55,32 @@ void *cliente(void *arg){
 
 int main(int argc, const char *argv[])
 {
+	if(argc != 5){
+		printf("%d\n",argc);
+		printf("USO: ./barbeiro <numero de barbeiros> <numero de cadeiras> <numero de clientes> <tipo 1-Cliente mais rapido 2-Barbeiro mais rapido 3-iguais>\n");
+		exit(1);
+	}
 	nBarbeiros = atoi(argv[1]);
 	nCadeiras = atoi(argv[2]);
 	nClientes = atoi(argv[3]);
+	tipo = atoi(argv[4]);
+
 	int i,j,aux;
-	
+
 	pthread_t barber[nBarbeiros];
 	pthread_t clients[nClientes];
-	
+
 	sem_init(&barbeiros_livres,0,nBarbeiros);
 	sem_init(&cadeiras_ocupadas,0,0);
-	
+
 	for (j = 0; j < nBarbeiros; j++) {
 		pthread_create(&barber[j],0,barbeiro,(void*)j);
 	}
 	for (i = 0; i < nClientes; i++) {
 		pthread_create(&clients[i],0,cliente,(void*)i);
-		sleep(1);
+		if(tipo == ClienteRapido) sleep(1);
+		else if(tipo == BarbeiroRapido) sleep(10);
+		else sleep(1);
 	}
 
 	for (j = 0; j < nBarbeiros; j++) {
@@ -73,6 +90,6 @@ int main(int argc, const char *argv[])
 	for (i = 0; i < nClientes; i++) {
 		pthread_join(clients[i],0);
 	}
-	
+
 	return 0;
 }
